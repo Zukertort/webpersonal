ba97187d725d456fa6635beed46e0a521f087ddd
Ricardo Gobbi <ricky.gobbi1994@gmail.com>
2024-07-23 12:28:07 -0300

    fueron agregadas tres views

--- core/views.py
+++ core/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Prueba</h1><h2>Portada</h2>")
+
+def about(request):
+    return HttpResponse("<h1>About me:</h1><p1>Hello, my name is Ricardo Gobbi and I'm a Python developer.</p1>")
+
+def portfolio(request):
+    return HttpResponse("<h1>My projects</h1><p1>This are my projects:</p1>")
+
+def contact(request):
+    return HttpResponse("<h1>Contacto</h1><p1>ricky.gobbi1994@gmail.com</p1>")
